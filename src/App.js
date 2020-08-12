ffa8a7bd4b007b26b210055b748545963b999971
Matthew Huus <matthew.huus3@gmail.com>
2020-08-11 20:46:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Matthew Huus',
+      headerLinks: [
+        {title:'Home', path:'/'},
+        {title:'About', path:'/about'},
+        {title:'Contact', path:'/contact'}
+      ],
+      home: {
+        title: 'Some title',
+        subTitle: 'some Subtitles about my projects',
+        text: 'something about my projs'
+      },
+      about: {
+        title: 'About Me',
+        
+      },
+      contact: {
+       title: 'Contact me!'
+      }
+    }
+  }
+  render() {
+    return (
+   <Router>
+     <Container fluid={true}>
+      Hello
+     </Container>
+     </Router>
   );
+  }
 }
 
 export default App;
