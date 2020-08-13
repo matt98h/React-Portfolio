4f7ad0b59a1c7d65e735dbc21b03ae6a98b12357
Matthew Huus <matthew.huus3@gmail.com>
2020-08-13 04:27:03 -0500

    working about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
+import Content from '../components/Content'
+
 function AboutPage(props) {
 return(
-<p>About Works!</p>
+<div>
+    <Hero title={props.title}/>
+
+    <Content>
+
+    </Content>
+</div>
+
 );
 }
 export default AboutPage
